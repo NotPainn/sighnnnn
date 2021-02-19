dea73d8950dacb846d6fcb2d7cc529ae8f935ea3
NotPainn <70415317+NotPainn@users.noreply.github.com>
2021-02-18 20:31:15 -0500

    z
    z

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('Discord.js');
 const client = new Discord.Client();
  
-////const { token,/ prefix, ServerID } = require('./config.json');
+const { token, prefix, ServerID } = require('./config.json');
  
 const fs = require('fs');
 const { userInfo } = require('os');
