ddd589e5edcdf152261c3820abd013c73d61c55c
NotPainn <70415317+NotPainn@users.noreply.github.com>
2021-02-18 19:53:23 -0500

    Hello
    Hellos

---
+++ commands/ban.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'ban',
+    description: "This command bans a member!",
+    execute(message, args){
+        const member = message.mentions.users.first();
+        if(member){
+            const memberTarger = message.guild.members.cache.get(member.id);
+            memberTarger.ban();
+            message.channel.send("User has been banned");
+        }else{
+            message.channel.send("Please tag a member you want to ban!");
+        }
+    }
+
+}
